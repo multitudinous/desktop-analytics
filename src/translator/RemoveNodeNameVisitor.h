f1471313d3fc4a50dd5a8ee5b8b5c425f4e4b179
Doug McCorkle <doug.mccorkle@gmail.com>
2013-07-30 18:24:18 +0000

    new translator to help generate the cad data for the desktop analytics tool.

---
+++ src/translator/RemoveNodeNameVisitor.h
@@ -0,0 +1,93 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2012 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date$
+ * Version:       $Rev$
+ * Author:        $Author$
+ * Id:            $Id$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.rb END do not edit this line> ***************/
+#ifndef REMOVE_NODE_NAME_VISITOR_H
+#define REMOVE_NODE_NAME_VISITOR_H
+
+/*!\file RemoveNodeNameVisitor.h
+ * \class ves::xplorer::scenegraph::util::RemoveNodeNameVisitor
+ *
+ */
+
+// --- OSG Includes --- //
+#include <osg/NodeVisitor>
+#include <osg/Group>
+#include <osg/Vec4>
+
+namespace ves
+{
+namespace xplorer
+{
+namespace scenegraph
+{
+namespace util
+{
+class RemoveNodeNameVisitor : public osg::NodeVisitor
+{
+public:
+    ///Constructor
+    ///\param node The node to be traversed
+    ///\param nodeName The name of the node to highlight
+    ///\param opaqueParent The hack to get around osg transparency issues
+    RemoveNodeNameVisitor( osg::Node* node, std::string prefix, std::string postfix );
+
+    ///Default Constructor
+    RemoveNodeNameVisitor();
+
+    ///Destructor
+    virtual ~RemoveNodeNameVisitor()
+    {
+        ;
+    }
+
+    ///Apply function that gets called during the traversal
+    ///\param node A parent node of the node being traversed
+    virtual void apply( osg::Node& node );
+
+
+private:
+    ///The name of the node to highlight
+    std::string mNodeName;
+    ///Ignore the case of the node name
+    bool m_ignoreCase;
+    ///prefix to remove
+    std::string m_prefix;
+    ///postfix to remove
+    std::string m_postfix;
+    ///node names to remove
+    std::string m_bodyName;
+};
+}
+}
+}
+}
+#endif //HIGHLIGHT_NODE_BY_NAME_VISITOR_H
---
+++ src/translator/main.cxx
@@ -0,0 +1,44 @@
+#include <osgDB/ReadFile>
+#include <osgDB/Registry>
+#include <osgDB/ReaderWriter>
+#include <osgDB/FileUtils>
+#include <osgDB/FileNameUtils>
+#include <osg/Node>
+
+#include <iostream>
+#include <string>
+
+#include <boost/filesystem/path.hpp>
+
+#include "RemoveNodeNameVisitor.h"
+
+int main( int argc, char* argv[] )
+{
+    if( argc < 2 )
+    {
+        std::cout << "Usage: jtToive <file to load> " << std::endl;
+        return 1;
+    }
+    //read in osg file
+    osg::ref_ptr< osg::Node > tempCADNode = osgDB::readNodeFile( argv[ 1 ] );
+    if( !tempCADNode.valid() )
+    {
+        std::cout << "Invalid file loaded" << std::endl;
+        return 1;
+    }
+    
+    boost::filesystem::path p( argv[ 1 ] );
+    p.replace_extension( "ive" );
+    //Remove crufty names from polytrans conversion
+    ves::xplorer::scenegraph::util::RemoveNodeNameVisitor polyTransCleanup( tempCADNode.get(), "", "" );
+
+    bool success = osgDB::writeNodeFile( *tempCADNode.get(), p.string() );
+    if( !success )
+    {
+        std::cout << "Unable to write converted jt file." << std::endl;
+    }
+
+    return 0;
+}
+
+
