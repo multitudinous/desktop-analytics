3353327be9bd2603b59c57a4c06705685f32c5e6
Aaron Bryden <abryden@praxik.com>
2013-10-28 19:49:29 +0000

    Test UI resizing  in feature demo. Testsuite is likely very broken right now.

--- DesktopAnalyticsTestSuite/src/jagmodel.h
+++ DesktopAnalyticsTestSuite/src/jagmodel.h
@@ -1,11 +1,12 @@
 #include "DemoInterface.h"
-
+#include <gmtl/Ray.h>
 class JagModel : public DemoInterface
 {
 public:
     JagModel()
       : DemoInterface( "jag.ex.jagmodel" ),
-        _fileName( "dumptruck.osg" )
+        _fileName( "memphis_accum.ive" ),
+		_imageName( "tex.png" )
 		
     {
 		currentAngle=0.0;
@@ -30,19 +31,28 @@ public:
 	virtual void toggleSecondNodeMask() {
 		secondNode->setNodeMask(!(secondNode->getNodeMask()));
 	}
+	
+	//virtual void performRayIntersection(gmtl::Ray<double> currentRay);
+	void JagModel::pickEvent(gmtl::Vec4d pos, int w, int h);
+	void doSomething() {
+		
+	}
 
 protected:
     gmtl::Matrix44d computeProjection( double aspect );
     gmtl::Matrix44d computeView();
 	gmtl::Matrix44d computeView( const double angleRad );
+	gmtl::Matrix44d lastProj;
 	double currentAngle;
-    std::string _fileName;
+    std::string _fileName, _imageName;
 
     jagSG::NodePtr _root;
 	jagSG::NodePtr secondNode;
+	jagSG::NodePtr thirdNode;
 	typedef jagDraw::PerContextData< double > PerContextAspect;
     PerContextAspect _aspect;
-
+	jagBase::TransformD tform;
     typedef jagDraw::PerContextData< gmtl::Matrix44d > PerContextMatrix44d;
     PerContextMatrix44d _proj;
+	jagDraw::NodeContainer _nodes;
 };
--- DesktopAnalyticsTestSuite/src/qtGlWidget.h
+++ DesktopAnalyticsTestSuite/src/qtGlWidget.h
@@ -19,12 +19,13 @@ public:
 protected:
     virtual void initializeGL();
     virtual void resizeGL( int w, int h );
+	virtual void rayEvent(float x, float y);
     virtual void paintGL();
 	
     virtual void keyPressEvent( QKeyEvent* e );
 	virtual void mouseMoveEvent(QMouseEvent* e);
 	virtual void mousePressEvent(QMouseEvent* e);
-	bool _leftDrag, _rightDrag;
+	bool _leftDrag, _rightDrag, _midDrag;
 	float _lastNX, _lastNY;
 	int _lastX, _lastY;
 
